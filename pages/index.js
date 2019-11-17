ed8ec129f8ee6085505c769546d781d26a781acf
Lorenzo Koundouris <lorenzo.koundouris@gmail.com>
2019-11-17 10:36:43 +0000

    fetching data and component lifecycle

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 
+import Page from '../components/Page';
+
 export default function Index() {
   return (
     <div>
@@ -7,16 +9,20 @@ export default function Index() {
       <Link href='/about'>
         <a>About</a>
       </Link>
+
       <h2>Cowsay</h2>
       <Link href='/cowsay'>
         <a>Cowsay</a>
       </Link>
+
       <h2>Dynamic routing</h2>
       {['id1', 'id2', 'id3'].map(pid => (
         <Link href='/post/[pid]' as={`/post/${pid}`}>
           <a>Post {pid}</a>
         </Link>
       ))}
+
+      <Page />
     </div>
   );
 }
